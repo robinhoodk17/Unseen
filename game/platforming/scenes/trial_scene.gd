d5fe6a7b89515008daa1ca4b0150c8390e2c27fe
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-06-16 08:45:02 +0200

    added music to the platformer and a grace period for when spotted after landing

--- game/platforming/scenes/trial_scene.gd
+++ game/platforming/scenes/trial_scene.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
-func free() -> void:
+func _ready() -> void:
+	await get_tree().create_timer(1).timeout
 	Ui.go_to("Game2D")
--- ui/game_2d/game_2d.gd
+++ ui/game_2d/game_2d.gd
@@ -1,7 +1,13 @@
 extends UiPage
 
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
 func _input(event: InputEvent) -> void:
 	if visible and event.is_action_pressed("ui_cancel"):
 		accept_event()
 		get_tree().paused = true
 		ui.go_to("PauseMenu")
+
+func show_ui():
+	if !audio_stream_player.playing:
+		audio_stream_player.play()
