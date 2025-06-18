91ed180c4f12cf7efcda8ef36603fb982c1e0f32
robinhoodk17 <fifthsunrevolution1@gmail.com>
2025-06-18 07:49:28 +0200

    player movement now supports collisions

--- game/driving/player/player_second_attempt.gd
+++ game/driving/player/player_second_attempt.gd
@@ -27,8 +27,10 @@ class_name player3d_second_attempt
 @export var curve_follow : Path3D
 
 var running : bool = false
+var setup : bool = false
 var current_speed : float = 0.0
 var previousBasis : Basis
+var correction_strength : float
 
 func _ready() -> void:
 	if curve_follow == null:
@@ -36,16 +38,24 @@ func _ready() -> void:
 	await Utils.reparent_node(camera_pivot,curve_follow,true)
 	camera_pivot.progress = 0
 	global_position = camera_pivot.global_position - global_basis.z * camera_offset
+	for i in 10:
+		position_camera()
+		global_position = camera_pivot.global_position - global_basis.z * camera_offset
 	running = true
+
+func position_camera() -> void :
+	var camera_point : Vector3 = curve_follow.curve.get_closest_point(global_position + mesh_container.global_basis.z * camera_offset)
+	var offset = curve_follow.curve.get_closest_offset(global_position)
+	var target_transform : Transform3D = curve_follow.curve.sample_baked_with_rotation(offset,false,true)
+	#print_debug(camera_point)
+	camera_pivot.position = camera_point
+	camera_pivot.basis = target_transform.basis
+	
 
 func _physics_process(delta: float) -> void:
 	if !running:
 		return
-	var camera_point : Vector3 = curve_follow.curve.get_closest_point(global_position + mesh_container.global_basis.z * camera_offset)
-	var offset = curve_follow.curve.get_closest_offset(global_position)
-	var target_transform : Transform3D = curve_follow.curve.sample_baked_with_rotation(offset,false,true)
-	camera_pivot.global_position = camera_point
-	camera_pivot.global_basis = target_transform.basis
+	position_camera()
 	if acceleration_input.value_bool:
 		current_speed += delta / acceleration_speed
 		if current_speed > 1.0:
@@ -54,37 +64,38 @@ func _physics_process(delta: float) -> void:
 		current_speed -= delta / braking_speed
 		if current_speed < 0.0:
 			current_speed = 0.0
-
+	
 	var z_component : float= -max_speed * speed_curve.sample(current_speed)
 	
 	var direction : Vector2 = direction_input.value_axis_2d
 	var x_component : float = direction.x * move_speed.x
 	var y_component : float = -direction.y * move_speed.y
 	mesh_container.look_at(camera_pivot.global_position - (camera_pivot.global_basis.z * 100))
-	var untransformed_vel : Vector3 = Vector3(x_component, y_component, z_component)
-	var transformed_vel : Vector3 = camera_pivot.global_basis * untransformed_vel
-	#if global_position * global_basis.x 
-	camera_3d.look_at(global_position)
-	velocity = transformed_vel
-	var relative_position : Vector3 = camera_pivot.global_position - global_position	
-	var relative_x : float = camera_pivot.global_basis.x.dot(relative_position)
+	
+	var relative_position : Vector3 = camera_pivot.global_position - global_position
+	var relative_x : float = -camera_pivot.global_basis.x.dot(relative_position)
 	var relative_y : float = relative_position.y
 	
 	#var wagon_relative_position : Vector3 = camera_pivot.global_position - current_wagon.global_position
 	#var wagon_relative_z : float = -camera_pivot.global_basis.z.dot(wagon_relative_position)
+	if relative_y <= -limits.y:
+		y_component = -2
+	if relative_y >= limits.y:
+		y_component = 2
 	
-	if relative_y < -limits.y:
-		y_component = 10
-	if relative_y > limits.y:
-		y_component = -10
-	
-	if relative_x < -limits.x:
-		x_component = 10
-	if relative_x > limits.x:
-		x_component = -10
-	
+	if relative_x <= -limits.x:
+		x_component = 2
+	if relative_x >= limits.x:
+		x_component = -2
 	#if wagon_relative_z < wagon_optimal_distance:
 		#pass
+	
+	var untransformed_vel : Vector3 = Vector3(x_component, y_component, z_component)
+	var transformed_vel : Vector3 = camera_pivot.global_basis * untransformed_vel
+	#if global_position * global_basis.x 
+	#camera_3d.look_at(global_position)
+	velocity = transformed_vel
+
 	handle_animations(delta, direction)
 	move_and_slide()
 
